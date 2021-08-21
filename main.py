9856e0527484ce5be720e7eb313dd1b633960e15
JonZavialov <74842207+JonZavialov@users.noreply.github.com>
2021-08-21 19:05:25 -0400

    added gallery and play page

--- main.py
+++ main.py
@@ -10,4 +10,8 @@ def home():
 def aboutus():
     return render_template('aboutus.html')
 
+@app.route("/play")
+def play():
+    return render_template('play.html')
+
 app.run(host = "0.0.0.0", port = 25565)
