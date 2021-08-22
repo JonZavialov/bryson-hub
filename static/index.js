eb8c14aa1b6792a93bf25803755f78556f2e3486
JonZavialov <74842207+JonZavialov@users.noreply.github.com>
2021-08-21 21:42:13 -0400

    added 404 page for invalid youtube ID

--- static/index.js
+++ static/index.js
@@ -1,8 +1,24 @@
 const PORT = 25565
 const URL = `http://localhost:${PORT}`
 
-function checkForID(id){
+function checkID(id){
     if(!id){
-        window.location.replace("/404");
+        window.location.replace("/404")
     }
+
+    let apiURL = `https://www.googleapis.com/youtube/v3/videos?part=id&id=${ id }&key=AIzaSyADFjypT-DMqC9M06DtWzCRtBy4Z_ucVco`
+    let response = httpGet(apiURL)
+    let videoPresent= response.pageInfo.totalResults
+    
+    if(videoPresent == 0){
+        window.location.replace("/404")
+    }
+}
+
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return JSON.parse(xmlHttp.responseText);
 }
