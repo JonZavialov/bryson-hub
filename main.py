1cb80f2d087688b5b7fbebac5ffb5ae808b28292
JonZavialov <74842207+JonZavialov@users.noreply.github.com>
2021-08-21 23:48:10 -0400

    added youtube url input and url parser

--- main.py
+++ main.py
@@ -10,13 +10,23 @@ def home():
 def aboutus():
     return render_template('aboutus.html')
 
-@app.route("/play")
-def play():
+@app.route("/watch")
+def watch():
     id = request.args.get('id', default = "", type = str)
-    return render_template('play.html', id=id)
+    return render_template('watch.html', id=id)
 
 @app.route("/404")
 def notFound():
-    return render_template('404.html')
+    error = request.args.get('error', default = "", type = str)
+    return render_template('404.html', error=error)
+
+@app.route("/play")
+def play():
+    return render_template('play.html')
+
+@app.route("/parse")
+def parse():
+    url = request.args.get('url', default = "", type = str)
+    return render_template('parse.html', url=url)
 
 app.run(host = "0.0.0.0", port = 25565)
--- static/index.js
+++ static/index.js
@@ -3,15 +3,15 @@ const URL = `http://localhost:${PORT}`
 
 function checkID(id){
     if(!id){
-        window.location.replace("/404")
-    }
-
-    let apiURL = `https://www.googleapis.com/youtube/v3/videos?part=id&id=${ id }&key=AIzaSyADFjypT-DMqC9M06DtWzCRtBy4Z_ucVco`
-    let response = httpGet(apiURL)
-    let videoPresent= response.pageInfo.totalResults
-    
-    if(videoPresent == 0){
-        window.location.replace("/404")
+        window.location.replace("/404?error=NoVideoID")
+    }else{
+        let apiURL = `https://www.googleapis.com/youtube/v3/videos?part=id&id=${ id }&key=AIzaSyADFjypT-DMqC9M06DtWzCRtBy4Z_ucVco`
+        let response = httpGet(apiURL)
+        let videoPresent= response.pageInfo.totalResults
+        
+        if(videoPresent == 0){
+            window.location.replace("/404?error=IDInvalid")
+        }
     }
 }
 
@@ -21,4 +21,17 @@ function httpGet(theUrl)
     xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
     xmlHttp.send( null );
     return JSON.parse(xmlHttp.responseText);
+}
+
+function parseURL(url){
+    let parsedURL
+    if(url.indexOf("?v=") != -1 && url.length == 43){
+        parsedURL = url.substring(url.indexOf("?v=")+3,url.length)
+        window.location.replace(`/watch?id=${parsedURL}`)
+    }else if(url.indexOf("?v=") != -1 && url.indexOf("&amp;ab") != -1){
+        parsedURL = url.substring(url.indexOf("?v=")+3,url.indexOf("&amp;ab"))
+        window.location.replace(`/watch?id=${parsedURL}`)
+    }else{
+        window.location.replace("/404?error=InvalidURL")
+    }
 }
